4905c0ec9348bcb4d1b078412cd985f2a27683be
initials101 <muriithidennis340@gmail.com>
2025-05-30 16:55:44 +0300

    updated commit the react frontend development port

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ connectDB()
 app.use(helmet())
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   }),
 )
